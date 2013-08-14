3ea9471da88b44af93d573e75d61422282272f8b
Hack Reactor Students <github@hackreactor.com>
2013-08-14 13:19:01 -0700

    implemented findNRooksSolution, first test passes

--- src/solvers.js
+++ src/solvers.js
@@ -3,28 +3,58 @@
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 
 window.findNRooksSolution = function(n){
-  var solution = undefined; //fixme
+  // if n is 3
+  // create a 3x3 chessboard
+  // [[0,0,0]
+  //  [0,0,0]
+  //  [0,0,0]]
+  // creates 3 rooks
+  var solution = [];
+  for(var i = 0; i < n; i++){
+    solution.push(new Array(n));
+  }
+
+  for(var j = 0; j < n.length; j++){
+    for(var k = 0; k < n.length; k++){
+      if(noRookCollisions(solution, j, k)){
+        solution[j][k] = 1;
+      }
+    }
+  }
+  var noRookCollisions = function(solution, yPos, xPos){
+    if(_.contains(solution[yPos], 1)){
+      return false;
+    } else {
+      for(var i = 0; i < solution[yPos].length; i++) {
+        if(solution[i][xPos] === 1){
+          return false;
+        }
+      }
+      return true;
+    }
+  };
+
 
   console.log('Single solution for ' + n + ' rooks:', solution);
   return solution;
 };
 
 window.countNRooksSolutions = function(n){
-  var solutionCount = undefined; //fixme
+  // var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 window.findNQueensSolution = function(n){
-  var solution = undefined; //fixme
+  // var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' queens:', solution);
   return solution;
 };
 
 window.countNQueensSolutions = function(n){
-  var solutionCount = undefined; //fixme
+  // var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
